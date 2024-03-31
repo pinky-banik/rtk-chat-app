12935077fcec4caa54cf208f51079487237fe62b
pinky-banik <pinky.rani.banik@gmail.com>
2024-03-31 21:25:53 +0600

    conversationapi

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Inbox from "./pages/Inbox";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import UseAuthCheck from "./Hooks/useAuthCheck";
+import PublicRoute from "./components/PublicRoutes";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
 
@@ -14,9 +16,9 @@ function App() {
     <Router>
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/inbox" element={<Conversation />} />
-      <Route path="/inbox/:id" element={<Inbox />} />
+      <Route path="/register" element={<PublicRoute><Register/></PublicRoute>} />
+      <Route path="/inbox" element={<PrivateRoute><Conversation /></PrivateRoute>} />
+      <Route path="/inbox/:id" element={<PrivateRoute><Inbox /></PrivateRoute>} />
     </Routes>
   </Router>
   );
